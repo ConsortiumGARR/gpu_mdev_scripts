bfb3ba0dd3f527540168cf3fff08fce8bc882922
emanuele <emanuele.petagna@garr.it>
2022-05-17 10:30:37 +0200

    Added saving at reboot and shutdown

--- nvidia_mdevs_saver.sh
+++ nvidia_mdevs_saver.sh
@@ -2,5 +2,9 @@
 
 SAVED_MDEVS=/etc/saved_nvidia_mdevs/nvidia_mdevs
 
+if [ ! -z "$1" ]; then
+	SAVED_MDEVS=$1
+fi
+
 mdevctl list | sort > $SAVED_MDEVS
 exit 0
--- nvidia_mdevs_undefined_restore.sh
+++ nvidia_mdevs_undefined_restore.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-SAVED_MDEVS=/etc/saved_nvidia_mdevs/nvidia_mdevs
+# Default saved mdevs file (from save_nvidia_mdevs.service)
+DEFAULT_SAVED_MDEVS=/etc/saved_nvidia_mdevs/nvidia_mdevs
+
+# Before clean reboot/shutdown nvidia mdevs are seved to this file..
+SHUTDOWN_REBOOT_SAVED_MDEVS=/etc/saved_nvidia_mdevs/nvidia_mdevs.reboot
+SAVED_MDEVS=""
 
 # You may need to adjust this variable..
 SYSPATH="/sys/devices/pci*"
 CREATE_PATH="/mdev_supported_types/nvidia-315/create"
 
+# Restore after a clean reboot/shutdown
+if [ -s "$SHUTDOWN_REBOOT_SAVED_MDEVS" ]; then
+    # restore from it
+    SAVED_MDEVS=$SHUTDOWN_REBOOT_SAVED_MDEVS
+else
+    SAVED_MDEVS=$DEFAULT_SAVED_MDEVS
+fi
+
+
 sleep 30
 #check if SAVED_MDEVS exists and is not empty..
 if [  -s "$SAVED_MDEVS" ]; then
+    echo "Current saved mdevs file: $SAVED_MDEVS"
     while IFS= read -r line; do
         # restore not defined mdevs..
         if [[ ! $line =~ defined ]]; then
@@ -18,6 +33,10 @@ if [  -s "$SAVED_MDEVS" ]; then
             echo $UUID > $GPU_PATH/$CREATE_PATH
         fi
     done < "$SAVED_MDEVS"
+    if [ -f "$SHUTDOWN_REBOOT_SAVED_MDEVS" ]; then
+        # delete clean reboot/shutdown mdevs file if it exists..
+        rm $SHUTDOWN_REBOOT_SAVED_MDEVS
+    fi
 else # no SAVED_MDEV file or empty..
     logger -p syslog.info "File $SAVED_MDEVS does not exist or is empty. Nothing to restore."
     exit 17
