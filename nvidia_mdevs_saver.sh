b7351e6c40104c2379a19e44523f003ceee94b38
emanuele <emanuele.petagna@garr.it>
2022-05-18 11:22:32 +0200

    if mdevctl returns no mdevs then we have a problem: don't overwrite the old list!

--- nvidia_mdevs_saver.sh
+++ nvidia_mdevs_saver.sh
@@ -2,9 +2,20 @@
 
 SAVED_MDEVS=/etc/saved_nvidia_mdevs/nvidia_mdevs
 
+# if alternative saving dir is specified as argument..use it
 if [ ! -z "$1" ]; then
 	SAVED_MDEVS=$1
 fi
 
-mdevctl list | sort > $SAVED_MDEVS
+# save mdevs (if any) into a variable..
+MDEVS=`mdevctl list | sort`
+# on a gpu machine we should always have some mdevs in use..
+# if there are errors on reboot or if something goes wrong
+# don't update the mdevs file, we just keep the last non-empty list..
+if [ -n "$MDEVS" ]; then
+    echo "$MDEVS" > $SAVED_MDEVS
+else
+    logger -p syslog.info "No mdevs found, something bad happened, check your configuration :("
+fi 
+
 exit 0
